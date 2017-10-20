786b943c6fe84b6180c46686f69e1d6e055dce09
Lucas de Azevedo Lima <Lucas.a.lima13@outlook.com>
2017-10-20 10:38:16 -0300

    Update funcoes_toff.h
    essa função não existe mais.

--- raspi/opencv/tracking/offline/funcoes_toff.h
+++ raspi/opencv/tracking/offline/funcoes_toff.h
@@ -10,8 +10,6 @@ void principal(char *argv[]);
 //função de modificação da imagem
 Mat transform(Mat frame);
 
-void Gravideo(Point center, Mat gravideo);
-
 //função de transformação, restrição e exibição da imagem capturada
 void frame(Mat frame, double fps);
 
