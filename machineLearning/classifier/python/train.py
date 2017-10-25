c20671e71042ea9975a47497b37cf4148b2db343
RIchard <richardson-santiago@hotmail.com>
2017-10-25 00:01:22 -0300

    Preparacao das imagens em python

--- machineLearning/classifier/python/train.py
+++ machineLearning/classifier/python/train.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from PIL import Image
+import cv2
 
 labels = []
 trainingFilenames = []
@@ -15,34 +15,19 @@ def gets_training_files():
             trainingFilenames.append(root+str(classe)+"/"+img)
             labels.append(classe)
 
-STANDARD_SIZE = (300, 300)
-def img_to_matrix(filename):
-    """
-    Pega um nome de arquivo e o transforma em um array numpy de pixels RGB
-    """
-    img = Image.open(filename)
-    img = img.resize(STANDARD_SIZE)
-    img = list(img.getdata())
-    img = map(list, img)
-    img = np.array(img)
-    return img
-
-def flatten_image(img):
-    """
-    Pega um (m, n) numpy array e o transforma em um array (1, m * n)
-    """
-    s = img.shape[0] * img.shape[1]
-    img_wide = img.reshape(1, s)
-    return img_wide[0]
-
 gets_training_files()
+
 data = []
-for file in trainingFilenames:
-    img = img_to_matrix(file)
-    img = flatten_image(img)
+
+print("Analizando rotulos....")
+for index, file in enumerate(trainingFilenames):
+    #print("Analizando rotulo (Classe|Arquivo)-> {}|{}".format(labels[index], trainingFilenames[index]))
+    img = cv2.imread(trainingFilenames[index],0)
     data.append(img)
-
-data = np.array(data)
-print data 
-
+    
+'''
+cv2.imshow('Mice',data[132])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
 
