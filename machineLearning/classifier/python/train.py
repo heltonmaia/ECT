957af44847392a33e11c200c9ba122f1226c3474
RIchard <richardson-santiago@hotmail.com>
2017-10-24 23:19:10 -0300

    algoritmo de classificação em python

--- machineLearning/classifier/python/train.py
+++ machineLearning/classifier/python/train.py
@@ -1,14 +1,48 @@
 import os
+import numpy as np
+from PIL import Image
 
 labels = []
 trainingFilenames = []
-root = "images/"
+root = "training_files/"
 
-def getImages():
+def gets_training_files():
+    """
+    Pega todas os nomes das aquivos em training_files
+    """
     for classe in os.listdir(root):    
         for img in os.listdir(root+str(classe)):
             trainingFilenames.append(root+str(classe)+"/"+img)
             labels.append(classe)
- 
-getImages()
-print labels
+
+STANDARD_SIZE = (300, 300)
+def img_to_matrix(filename):
+    """
+    Pega um nome de arquivo e o transforma em um array numpy de pixels RGB
+    """
+    img = Image.open(filename)
+    img = img.resize(STANDARD_SIZE)
+    img = list(img.getdata())
+    img = map(list, img)
+    img = np.array(img)
+    return img
+
+def flatten_image(img):
+    """
+    Pega um (m, n) numpy array e o transforma em um array (1, m * n)
+    """
+    s = img.shape[0] * img.shape[1]
+    img_wide = img.reshape(1, s)
+    return img_wide[0]
+
+gets_training_files()
+data = []
+for file in trainingFilenames:
+    img = img_to_matrix(file)
+    img = flatten_image(img)
+    data.append(img)
+
+data = np.array(data)
+print data 
+
+
