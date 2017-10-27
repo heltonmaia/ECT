73ba6dcc4692f86efe627a228ecc0b6fda314974
RIchard <richardson-santiago@hotmail.com>
2017-10-27 10:41:51 -0300

    treinamento em python

--- machineLearning/classifier/python/train.py
+++ machineLearning/classifier/python/train.py
@@ -1,9 +1,11 @@
 import os
 import numpy as np
 import cv2
+import time
 
 labels = []
 trainingFilenames = []
+imgs = []
 root = "training_files/"
 
 def gets_training_files():
@@ -15,16 +17,51 @@ def gets_training_files():
             trainingFilenames.append(root+str(classe)+"/"+img)
             labels.append(classe)
 
+def SVMParams(svm):
+    print("Parametros da SVM:\n\
+           Kernel type     :{}\n\
+           Type            :{}\n\
+           C               :{}\n\
+           Degree          :{}\n\
+           Nu              :{}\n\
+           Gamma           :{}\n\
+    ".format(svm.getKernelType(), svm.getType(), svm.getC(), svm.getDegree(), svm.getNu(), svm.getGamma()))
+
+
 gets_training_files()
 
-data = []
-
-print("Analizando rotulos....")
-for index, file in enumerate(trainingFilenames):
+i = time.clock()
+print("\nAnalizando rotulos....")
+for index, file in enumerate(trainingFilenames):    
     #print("Analizando rotulo (Classe|Arquivo)-> {}|{}".format(labels[index], trainingFilenames[index]))
-    img = cv2.imread(trainingFilenames[index],0)
-    data.append(img)
-    
+    img = cv2.imread(trainingFilenames[index], 0)
+    imgs.append(img.flatten())
+imgs = np.array(imgs, dtype=np.float32)
+labels = np.array(labels, dtype=np.int32)
+f = time.clock()
+print("A analise dos rotulos levou: {}s".format(f-i))
+
+
+#Configura a SVM
+#Seta os melhores parametros 
+svm = cv2.ml.SVM_create()
+svm.setType(cv2.ml.SVM_C_SVC)
+svm.setKernel(cv2.ml.SVM_POLY)
+svm.setC(1)
+svm.setGamma(0.50625)
+svm.setDegree(3)
+
+i = time.clock()
+print("Treinando o classificador.......")
+svm.train(imgs, cv2.ml.ROW_SAMPLE, labels)
+f = time.clock()
+print("O treinamento levou: {}s".format(f-i))
+
+#Salva a SVM
+print("Salvando a SVM.......")
+svm.save("rats_and_pitons.yml")
+SVMParams(svm)
+  
 '''
 cv2.imshow('Mice',data[132])
 cv2.waitKey(0)
