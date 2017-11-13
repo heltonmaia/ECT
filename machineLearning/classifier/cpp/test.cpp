ef84fe536ef06bee024befd7ef88840cb11e7ff0
vanluwin <richardson-santiago@hotmail.com>
2017-11-13 11:47:29 -0300

    add carregamento de arquivos

--- machineLearning/classifier/cpp/test.cpp
+++ machineLearning/classifier/cpp/test.cpp
@@ -2,17 +2,10 @@
 #include <iostream>
 #include <iomanip>  
 #include <vector>
-#include <string>
 
 // OpenCV
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
 #include <opencv2/ml.hpp>
-#include "opencv2/objdetect.hpp"
-
-// TinyDir
-#include "extra_libs/tinydir.h"
 
 // Testes do tempo de execucao
 #include <ctime>
@@ -20,146 +13,35 @@
 using namespace std;
 using namespace cv;
 
-// Rotulos dos arquivos de treino
-vector<int> expectedLabels;
-vector<int> predictedLabels;
-vector<string> testFileNames;
-
-//Pega os arquivos e teste e salva os rotulos/arquvios em uma classe vector
-void getTestFiles(){
-
-    //Abre o diretorio que contem as imagens
-    tinydir_dir test_root_dir;
-    tinydir_open(&test_root_dir, "test_files");
-
-    // iterate over directories
-    while (test_root_dir.has_next){
-        // pega o arquivo
-        tinydir_file file;
-        tinydir_readfile(&test_root_dir, &file);
-
-        // se for um diretorio
-        if (file.is_dir){
-
-            string numbersDirName = file.name;
-
-            // pula . / .. / .DS_Store (OSX)
-            if (numbersDirName != "." && numbersDirName != ".." && numbersDirName != ".DS_Store"){
-
-                // atoi nao é muito seguro, porem suficiente para testes
-                int currentLabel = atoi(file.name);
-
-                // pega o diretorio com os aquivos de numeros
-                numbersDirName.insert(0, "test_files/");
-
-                // abre o diretorio que contem os numeros
-                tinydir_dir test_number_dir;
-                tinydir_open(&test_number_dir, numbersDirName.c_str());
-
-                // itera pelo diretorio
-                while (test_number_dir.has_next){
-                    //pega o aquivo
-                    tinydir_file testJpgFile;
-                    tinydir_readfile(&test_number_dir, &testJpgFile);
-
-                    // pega o nome
-                    string testJpgFileName = testJpgFile.name;
-
-                    // pula . / .. / .DS_Store (OSX)
-                    if (testJpgFileName != "." && testJpgFileName != ".." && testJpgFileName != ".DS_Store"){
-
-                        // diretorio foi iterado completament
-                        testJpgFileName.insert(0, numbersDirName + "/");
-
-                        //salva  o nome do arquivo de teste e seu rotulo
-                        testFileNames.push_back(testJpgFileName);
-                        expectedLabels.push_back(currentLabel);
-                    }
-
-                    // pega o proximo arquivo
-                    tinydir_next(&test_number_dir);
-                }
-
-                // fecha o diretorio
-                tinydir_close(&test_number_dir);
-            }
-        }
-
-        // pega o proximo arquivo
-        tinydir_next(&test_root_dir);
-    }
-
-    // close directory
-    tinydir_close(&test_root_dir);
-}
-
-//Cria o descritor HOG
-HOGDescriptor hog(
-    Size(300,300), //winSize, tamanho das imagens
-    Size(40,40), //blocksize, lida com variacoes de iluminacao (2 x cellSize - normalmente)
-    Size(20,20), //blockStride, controla o grau de normalização do contraste (0.5 x blockSize)
-    Size(20,20), //cellSize, tamanho da descritor que é escolhido com base na escala dos recursos importantes para fazer a classificação 
-             9, //nbins
-              1, //derivAper
-             -1, //winSigma
-              0, //histogramNormType
-            0.2, //L2HysThresh
-              1,//gammal correction
-              64,//nlevels=64
-              1//Use signed gradients 
-);
-
-//Cria os descritores HOG das imagens
-void createTestHOG(vector<vector<float>> &testHOG, vector<string> testFileNames){
-    
-    for (int index = 0; index < testFileNames.size(); index++){
-        // le a imagem(grayscale)
-        Mat imgMat = imread(testFileNames[index], 0);
-
-        vector<float> descriptors;
-        hog.compute(imgMat, descriptors);
-        testHOG.push_back(descriptors);
-    }
-        
-}
-
-//converte os descritores em matrizes
-void convertVectortoMatrix(vector<vector<float>> &testHOG, Mat &testMat, int descriptor_size){
-
-    for(int i = 0;i<testHOG.size();i++){
-        for(int j = 0;j<descriptor_size;j++){
-            testMat.at<float>(i,j) = testHOG[i][j]; 
-        }
-    }
-
-}
-
-
-void SVMtest(Mat &testMat, Mat &testResponse){
+void SVMpredict(Mat &testMat, Mat &testResponse){
 
     //Carrega a SVM
     cout << "Carregando a svm ...\n";
-    Ptr<ml::SVM> svm = ml::StatModel::load<ml::SVM>("HOG_RATS.yml");
+    Ptr<ml::SVM> svm = ml::StatModel::load<ml::SVM>("ymls/HOG_RATS_POLY.yml");
 
+    cout << "Classificando ...\n";
     svm->predict(testMat, testResponse); 
+    cout << "Salvando as classificacoes ...\n";
+    FileStorage fs("ymls/testResponse.yml", FileStorage::WRITE);
+    fs << "testResponse" << testResponse;
+    fs.release(); 
 }
 
-void SVMevaluate(Mat &testResponse, vector<int> expectedLabels){
+void SVMevaluate(Mat &testResponse, vector<int> testLabels){
     
     // Informacoes
-    int totalClassifications = 0;
-    int totalCorrect = 0;
+    int totalClassifications = 0, totalCorrect = 0, totalWrong = 0;
 
     for(int i = 0; i < testResponse.rows; i++){
-        cout << "Predito: "<< setw(2) << testResponse.at<float>(i,0) << " | Esperado: " << expectedLabels[i] << endl;
-        if(testResponse.at<float>(i,0) == expectedLabels[i]){
+        //cout << "Predito: "<< setw(2) << testResponse.at<float>(i,0) << " | Esperado: " << testLabels[i] << endl;
+        if(testResponse.at<float>(i,0) == testLabels[i]){
             totalCorrect++;
+        }else{
+            totalWrong++;
         } 
         totalClassifications++;
     }
-    
-    int totalWrong = totalClassifications - totalCorrect;
-    
+       
     float percentageCorrect = ((float)totalCorrect / totalClassifications) * 100;
     float percentageIncorrect = 100 - percentageCorrect;
     
@@ -174,25 +56,22 @@ void SVMevaluate(Mat &testResponse, vector<int> expectedLabels){
 
 int main(int argc, char **argv){
     cout << "******Testando o classificador******\n";
+
+    cout << "Lendo os dados ...\n";
+    FileStorage fs("ymls/testMat.yml", FileStorage::READ);
+
+    vector<int> testLabels;
+    fs["testLabels"] >> testLabels;
+
+    Mat testMat, testResponse;
+    fs["testMat"] >> testMat;
+    fs.release(); 
     
-    cout << "Adquirindo arquivos ...\n";
-    //Pega os arquivos de teste
-    getTestFiles();
-
-    cout << "Criando os descritores HOG ...\n";
-    vector<vector<float>> testHOG;
-    createTestHOG(testHOG, testFileNames);
-
-    int descriptor_size = testHOG[0].size();
-    cout << "Tamanho dos descritores: " << descriptor_size << endl;
-
-    Mat testMat(testHOG.size(),descriptor_size,CV_32FC1);
-    convertVectortoMatrix(testHOG, testMat, descriptor_size);
-
-    Mat testResponse;
-    SVMtest(testMat, testResponse);
-        
-    SVMevaluate(testResponse, expectedLabels);
+    //classifica as imagens de teste
+    SVMpredict(testMat, testResponse);
+   
+    //avalia as classificações feitas
+    SVMevaluate(testResponse, testLabels);
     
     return 0;
 }
--- machineLearning/classifier/cpp/train.cpp
+++ machineLearning/classifier/cpp/train.cpp
@@ -1,133 +1,16 @@
 // Bibliotecas Padrao
 #include <iostream>
 #include <vector>
-#include <string>
 
 // Testes do tempo de execucao
 #include <ctime>
 
 // OpenCV
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
 #include <opencv2/ml.hpp>
-#include "opencv2/objdetect.hpp"
-
-// TinyDir
-#include "extra_libs/tinydir.h"
 
 using namespace std;
 using namespace cv;
-
-// Rotulos dos arquivos de treino
-vector<int> labels; 
-vector<string> trainingFilenames;
-
-//Pega os arquivos e teste e salva os rotulos/arquvios em uma classe vector
-void getsTrainingFiles(){
-    
-        //Abre o diretorio que contem as imagens 
-        tinydir_dir training_root_dir;
-        tinydir_open(&training_root_dir, "training_files");
-    
-        // itera pelo diretorio 
-        while (training_root_dir.has_next){
-            // pega o arquivo
-            tinydir_file file;
-            tinydir_readfile(&training_root_dir, &file);
-    
-            // se for um diretorio 
-            if (file.is_dir){
-    
-                string numbersDirName = file.name;
-    
-                // pula . / .. / .DS_Store (OSX)
-                if (numbersDirName != "." && numbersDirName != ".." && numbersDirName != ".DS_Store"){
-    
-                    // atoi nao é muito seguro, porem suficiente para testes 
-                    int currentLabel = atoi(file.name);
-    
-                    // pega o diretorio com os aquivos de numeros
-                    numbersDirName.insert(0, "training_files/");
-    
-                    // abre o diretorio que contem os numeros
-                    tinydir_dir training_number_dir;
-                    tinydir_open(&training_number_dir, numbersDirName.c_str());
-    
-                    // itera pelo diretorio
-                    while (training_number_dir.has_next) {
-                        //pega o aquivo
-                        tinydir_file trainingJpgFile;
-                        tinydir_readfile(&training_number_dir, &trainingJpgFile);
-    
-                        // pega o nome
-                        string trainingJpgFileName = trainingJpgFile.name;
-    
-                        // pula . / .. / .DS_Store (OSX)
-                        if (trainingJpgFileName != "." && trainingJpgFileName != ".." && trainingJpgFileName != ".DS_Store"){
-    
-                            // diretorio foi iterado completamente
-                            trainingJpgFileName.insert(0, numbersDirName + "/");
-    
-                            //salva  o nome do arquivo de treinamento e seu rotulo
-                            trainingFilenames.push_back(trainingJpgFileName);
-                            labels.push_back(currentLabel);
-                        }
-    
-                        // pega o proximo arquivo
-                        tinydir_next(&training_number_dir);
-                    }
-    
-                    // fecha o diretorio
-                    tinydir_close(&training_number_dir);
-                }
-            }
-    
-            //pega o proximo arquivo
-            tinydir_next(&training_root_dir);
-        }
-    
-    // fecha o diretorio
-    tinydir_close(&training_root_dir);
-}
-
-HOGDescriptor hog(
-    Size(300,300), //winSize, tamanho das imagens
-    Size(40,40), //blocksize, lida com variacoes de iluminacao (2 x cellSize - normalmente)
-    Size(20,20), //blockStride, controla o grau de normalização do contraste (0.5 x blockSize)
-    Size(20,20), //cellSize, tamanho da descritor que é escolhido com base na escala dos recursos importantes para fazer a classificação 
-             9, //nbins
-              1, //derivAper
-             -1, //winSigma
-              0, //histogramNormType
-            0.2, //L2HysThresh
-              1,//gammal correction
-              64,//nlevels=64
-              1//Use signed gradients 
-);
-
-//Cria os descritores HOG das imagens
-void createTrainHOG(vector<vector<float>> &trainHOG, vector<string> trainingFilenames){
-
-    for (int index = 0; index < trainingFilenames.size(); index++){
-        // le a imagem(grayscale)
-        Mat imgMat = imread(trainingFilenames[index], 0);
-
-        vector<float> descriptors;
-        hog.compute(imgMat, descriptors);
-        trainHOG.push_back(descriptors);
-    }
-    
-}
-
-//converte os descritores em matrizes
-void convertVectortoMatrix(vector<vector<float>> &trainHOG, Mat &trainMat, int descriptor_size){
-    for(int i = 0;i<trainHOG.size();i++){
-        for(int j = 0;j<descriptor_size;j++){
-           trainMat.at<float>(i,j) = trainHOG[i][j]; 
-        }
-    }
-}
 
 //Mostra os parametros da SVM
 void SVMParams(ml::SVM *svm){
@@ -142,30 +25,23 @@ void SVMParams(ml::SVM *svm){
 //Todas as ações da SVM
 void SVMtrain(Mat &trainMat,Mat &labelsMat){    
     // Configura a SVM
-    // 'Seta' os parametros(optimal(ish)) da SVM's    
     //Kernel do tipo polinomial de grau 3 (best one so far)
+    
     Ptr<ml::SVM> svm = cv::ml::SVM::create();
     svm->setKernel(ml::SVM::POLY);
 	svm->setType(ml::SVM::C_SVC);
-    svm->setC(12.5);
+    svm->setC(1);
     svm->setDegree(3);
-    svm->setGamma(1e-05);
-    /*
-    best:
-    Numero de classificacoes: 57
-    Corretas:  34 (59.6491%)
-    Erradas: 23 (40.3509%)
-    */
+    svm->setGamma(0.50625);
     
     /*
-    //Kernel do tipo RBF (nao apresentou bons resultados)
+    //Kernel do tipo RBF 
     Ptr<ml::SVM> svm = ml::SVM::create();
     svm->setType(ml::SVM::C_SVC);
     svm->setKernel(ml::SVM::RBF);
     svm->setGamma(0.50625);
     svm->setC(12.5);
     */
-
     /*
     //kernel do tipo CHI2
     Ptr<ml::SVM> svm = ml::SVM::create();
@@ -175,13 +51,6 @@ void SVMtrain(Mat &trainMat,Mat &labelsMat){
     svm->setC(2.5);
     svm->setGamma(1e-5);
     */
-    /*
-    best:
-    Numero de classificacoes: 57
-    Corretas:  19 (33.3333%)
-    Erradas: 38 (66.6667%)
-    */
-    
     //Treina o classificador 
     int i = clock();
     cout << "Treinando o classificador ...\n";
@@ -192,7 +61,7 @@ void SVMtrain(Mat &trainMat,Mat &labelsMat){
 
     // Salva a SVM
     cout << "Salvando a SVM ...\n";
-    svm->save("HOG_RATS.yml");
+    svm->save("ymls/HOG_RATS_POLY.yml");
     cout <<  endl;
     SVMParams(svm);
 }
@@ -201,28 +70,12 @@ void SVMtrain(Mat &trainMat,Mat &labelsMat){
 int main(int argc, char **argv){
     cout << "******Classificacao de Imagens******\n";
 
-    cout << "Adquirindo arquivos ...\n";
-    // pega os arquivos de treino
-    getsTrainingFiles();
-
-    cout << "Criando os descritores HOG ...\n";
-    vector<vector<float>> trainHOG;    
-    createTrainHOG(trainHOG, trainingFilenames);
-
-    int descriptor_size = trainHOG[0].size();
-    cout << "Tamanho dos descritores: " << descriptor_size << endl;
-
-    Mat trainMat(trainHOG.size(),descriptor_size,CV_32FC1);
-    convertVectortoMatrix(trainHOG, trainMat, descriptor_size);
-
-    //Processa os rotulos 
-    int labelsArray[labels.size()];    
-    // itera pelos rotulos
-    for (int index = 0; index < labels.size(); index++){
-        labelsArray[index] = labels[index];
-    }
-    
-    Mat labelsMat(labels.size(), 1, CV_32S, labelsArray);
+    cout << "Lendo os dados ...\n";
+    FileStorage fs("ymls/trainMat.yml", FileStorage::READ);
+    Mat trainMat, labelsMat;
+    fs["labelsMat"] >> labelsMat;
+    fs["trainMat"] >> trainMat;
+    fs.release(); 
 
     SVMtrain(trainMat, labelsMat);
 
