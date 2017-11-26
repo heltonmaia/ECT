0160d65d14feb7e1dc905f084423b43e0c2bef98
vanluwin <richardson-santiago@hotmail.com>
2017-11-26 15:31:13 -0300

    compila todos os arquivos

--- machineLearning/classifier/cpp/compilaTudo.sh
+++ machineLearning/classifier/cpp/compilaTudo.sh
@@ -2,3 +2,4 @@ echo "Compilando tudo ..."
 g++ -ggdb train.cpp -o train `pkg-config --cflags --libs opencv`
 g++ -ggdb test.cpp -o test `pkg-config --cflags --libs opencv`
 g++ -ggdb hog.cpp -o hog `pkg-config --cflags --libs opencv`
+g++ -ggdb edges.cpp -o edges `pkg-config --cflags --libs opencv`
