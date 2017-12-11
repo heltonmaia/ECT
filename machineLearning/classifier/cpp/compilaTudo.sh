d825c4ac1c5cc038e093b33e97ef1e47f52aa4db
vanluwin <richardson-santiago@hotmail.com>
2017-12-11 15:36:29 -0300

    update

--- machineLearning/classifier/cpp/compilaTudo.sh
+++ machineLearning/classifier/cpp/compilaTudo.sh
@@ -3,3 +3,6 @@ g++ -ggdb train.cpp -o train `pkg-config --cflags --libs opencv`
 g++ -ggdb test.cpp -o test `pkg-config --cflags --libs opencv`
 g++ -ggdb hog.cpp -o hog `pkg-config --cflags --libs opencv`
 g++ -ggdb edges.cpp -o edges `pkg-config --cflags --libs opencv`
+
+g++ -ggdb sampling/samples_hog.cpp -o sampling/hog `pkg-config --cflags --libs opencv`
+g++ -ggdb sampling/samples_test.cpp -o sampling/test `pkg-config --cflags --libs opencv`
