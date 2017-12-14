124d4add027aaf2e7b2e512bcced0f405b037b04
vanluwin <richardson-santiago@hotmail.com>
2017-12-14 16:18:17 -0300

    .

--- machineLearning/classifier/cpp/installOpencv.sh
+++ machineLearning/classifier/cpp/installOpencv.sh
@@ -4,27 +4,10 @@ sudo apt-get update && sudo apt-get upgrade
 echo "Install OS libraries\n" 
 sudo apt-get remove x264 libx264-dev
 
-sudo apt-get install build-essential checkinstall cmake pkg-config yasm &&
-sudo apt-get install git gfortran &&
-sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev &&
-sudo apt-get install libtiff5-dev &&
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev &&
-sudo apt-get install libxine2-dev libv4l-dev &&
-sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev &&
-sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev &&
-sudo apt-get install libatlas-base-dev &&
-sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev &&
-sudo apt-get install libvorbis-dev libxvidcore-dev &&
-sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev &&
-sudo apt-get install x264 v4l-utils &&
-sudo apt-get install libprotobuf-dev protobuf-compiler &&
-sudo apt-get install libgoogle-glog-dev libgflags-dev &&
-sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen &&
+sudo apt-get install build-essential checkinstall cmake pkg-config yasm git gfortran libjpeg8-dev libjasper-dev libpng12-dev libtiff5-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev qt5-default libgtk2.0-dev libtbb-dev libatlas-base-dev libfaac-dev libmp3lame-dev libtheora-dev libvorbis-dev libxvidcore-dev libopencore-amrnb-dev libopencore-amrwb-dev x264 v4l-utils libprotobuf-dev protobuf-compiler libgoogle-glog-dev libgflags-dev libgphoto2-dev libeigen3-dev libhdf5-dev doxygen 
 
 echo "Install Python libraries\n"
-sudo apt-get install python-dev python-pip python3-dev python3-pip &&
-sudo -H pip2 install -U pip numpy &&
-sudo -H pip3 install -U pip numpy &&
+sudo apt-get install python-dev python-pip python3-dev python3-pip -H pip2 install -U pip numpy -H pip3 install -U pip numpy
 
 echo "Download opencv from Github\n"
 git clone https://github.com/opencv/opencv_contrib.git
