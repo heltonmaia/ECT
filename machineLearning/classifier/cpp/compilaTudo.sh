a28563b67e4e9c8462a1fd1b34c09df3dc32560f
vanluwin <richardson-santiago@hotmail.com>
2017-12-26 21:06:50 -0300

    script para compilar

--- machineLearning/classifier/cpp/compilaTudo.sh
+++ machineLearning/classifier/cpp/compilaTudo.sh
@@ -4,5 +4,5 @@ g++ -ggdb -std=c++11 test.cpp -o test `pkg-config --cflags --libs opencv`
 g++ -ggdb -std=c++11 hog.cpp -o hog `pkg-config --cflags --libs opencv`
 g++ -ggdb -std=c++11 edges.cpp -o edges `pkg-config --cflags --libs opencv`
 
-g++ -ggdb -std=c++11 sampling/samples_hog.cpp -o sampling/hog `pkg-config --cflags --libs opencv`
-g++ -ggdb -std=c++11 sampling/samples_test.cpp -o sampling/test `pkg-config --cflags --libs opencv`
+#g++ -ggdb -std=c++11 sampling/samples_hog.cpp -o sampling/hog `pkg-config --cflags --libs opencv`
+#g++ -ggdb -std=c++11 sampling/samples_test.cpp -o sampling/test `pkg-config --cflags --libs opencv`
