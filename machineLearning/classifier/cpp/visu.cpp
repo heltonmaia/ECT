2f68b7b441c1636271b31deb420fe0ce0c937fb2
vanluwin <richardson-santiago@hotmail.com>
2018-01-02 18:19:57 -0300

    criar vizulizações das imagens vistas pela svm

---
+++ machineLearning/classifier/cpp/visu.cpp
@@ -0,0 +1,255 @@
+// Bibliotecas Padrao
+#include <iostream>
+#include <vector>
+#include <string> 
+
+// OpenCV
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include "opencv2/objdetect.hpp"
+
+using namespace std;
+using namespace cv;
+
+/*
+The code below takes a HOG descriptor and computes the gradient magnitude per cell by looping through 
+all blocks and adding the gradient strengths for each of the cells per block to each individual cell 
+and finally averages the gradient strenghts.
+
+The gradient strengths are then visualized by line lengths in the direction of the corresponding 
+gradient bin.
+*/
+Mat get_hogdescriptor_visu(const Mat& color_origImg, vector<float>& descriptorValues, const Size & size )
+{
+    const int DIMX = size.width;
+    const int DIMY = size.height;
+    float zoomFac = 1;
+    Mat visu;
+    resize(color_origImg, visu, Size( (int)(color_origImg.cols*zoomFac), (int)(color_origImg.rows*zoomFac) ) );
+ 
+    int cellSize        = 20;
+    int gradientBinSize = 9;
+    float radRangeForOneBin = (float)(CV_PI/(float)gradientBinSize); // dividing 180 into 9 bins, how large (in rad) is one bin?
+ 
+    // prepare data structure: 9 orientation / gradient strenghts for each cell
+    int cells_in_x_dir = DIMX / cellSize;
+    int cells_in_y_dir = DIMY / cellSize;
+    float*** gradientStrengths = new float**[cells_in_y_dir];
+    int** cellUpdateCounter   = new int*[cells_in_y_dir];
+    for (int y=0; y<cells_in_y_dir; y++)
+    {
+        gradientStrengths[y] = new float*[cells_in_x_dir];
+        cellUpdateCounter[y] = new int[cells_in_x_dir];
+        for (int x=0; x<cells_in_x_dir; x++)
+        {
+            gradientStrengths[y][x] = new float[gradientBinSize];
+            cellUpdateCounter[y][x] = 0;
+ 
+            for (int bin=0; bin<gradientBinSize; bin++)
+                gradientStrengths[y][x][bin] = 0.0;
+        }
+    }
+ 
+    // nr of blocks = nr of cells - 1
+    // since there is a new block on each cell (overlapping blocks!) but the last one
+    int blocks_in_x_dir = cells_in_x_dir - 1;
+    int blocks_in_y_dir = cells_in_y_dir - 1;
+ 
+    // compute gradient strengths per cell
+    int descriptorDataIdx = 0;
+    int cellx = 0;
+    int celly = 0;
+ 
+    for (int blockx=0; blockx<blocks_in_x_dir; blockx++)
+    {
+        for (int blocky=0; blocky<blocks_in_y_dir; blocky++)
+        {
+            // 4 cells per block ...
+            for (int cellNr=0; cellNr<4; cellNr++)
+            {
+                // compute corresponding cell nr
+                cellx = blockx;
+                celly = blocky;
+                if (cellNr==1) celly++;
+                if (cellNr==2) cellx++;
+                if (cellNr==3)
+                {
+                    cellx++;
+                    celly++;
+                }
+ 
+                for (int bin=0; bin<gradientBinSize; bin++)
+                {
+                    float gradientStrength = descriptorValues[ descriptorDataIdx ];
+                    descriptorDataIdx++;
+ 
+                    gradientStrengths[celly][cellx][bin] += gradientStrength;
+ 
+                } // for (all bins)
+ 
+ 
+                // note: overlapping blocks lead to multiple updates of this sum!
+                // we therefore keep track how often a cell was updated,
+                // to compute average gradient strengths
+                cellUpdateCounter[celly][cellx]++;
+ 
+            } // for (all cells)
+ 
+ 
+        } // for (all block x pos)
+    } // for (all block y pos)
+ 
+ 
+    // compute average gradient strengths
+    for (celly=0; celly<cells_in_y_dir; celly++)
+    {
+        for (cellx=0; cellx<cells_in_x_dir; cellx++)
+        {
+ 
+            float NrUpdatesForThisCell = (float)cellUpdateCounter[celly][cellx];
+ 
+            // compute average gradient strenghts for each gradient bin direction
+            for (int bin=0; bin<gradientBinSize; bin++)
+            {
+                gradientStrengths[celly][cellx][bin] /= NrUpdatesForThisCell;
+            }
+        }
+    }
+ 
+    // draw cells
+    for (celly=0; celly<cells_in_y_dir; celly++)
+    {
+        for (cellx=0; cellx<cells_in_x_dir; cellx++)
+        {
+            int drawX = cellx * cellSize;
+            int drawY = celly * cellSize;
+ 
+            int mx = drawX + cellSize/2;
+            int my = drawY + cellSize/2;
+ 
+            rectangle(visu, Point((int)(drawX*zoomFac), (int)(drawY*zoomFac)), Point((int)((drawX+cellSize)*zoomFac), (int)((drawY+cellSize)*zoomFac)), Scalar(100,100,100), 1);
+ 
+            // draw in each cell all 9 gradient strengths
+            for (int bin=0; bin<gradientBinSize; bin++)
+            {
+                float currentGradStrength = gradientStrengths[celly][cellx][bin];
+ 
+                // no line to draw?
+                if (currentGradStrength==0)
+                    continue;
+ 
+                float currRad = bin * radRangeForOneBin + radRangeForOneBin/2;
+ 
+                float dirVecX = cos( currRad );
+                float dirVecY = sin( currRad );
+                float maxVecLen = (float)(cellSize/2.f);
+                float scale = 2.5; // just a visualization scale, to see the lines better
+ 
+                // compute line coordinates
+                float x1 = mx - dirVecX * currentGradStrength * maxVecLen * scale;
+                float y1 = my - dirVecY * currentGradStrength * maxVecLen * scale;
+                float x2 = mx + dirVecX * currentGradStrength * maxVecLen * scale;
+                float y2 = my + dirVecY * currentGradStrength * maxVecLen * scale;
+ 
+                // draw gradient visualization
+                line(visu, Point((int)(x1*zoomFac),(int)(y1*zoomFac)), Point((int)(x2*zoomFac),(int)(y2*zoomFac)), Scalar(0,255,0), 1);
+ 
+            } // for (all bins)
+ 
+        } // for (cellx)
+    } // for (celly)
+ 
+ 
+    // don't forget to free memory allocated by helper data structures!
+    for (int y=0; y<cells_in_y_dir; y++)
+    {
+        for (int x=0; x<cells_in_x_dir; x++)
+        {
+            delete[] gradientStrengths[y][x];
+        }
+        delete[] gradientStrengths[y];
+        delete[] cellUpdateCounter[y];
+    }
+    delete[] gradientStrengths;
+    delete[] cellUpdateCounter;
+ 
+    return visu;
+ 
+} // get_hogdescriptor_visu
+
+HOGDescriptor hog(
+    Size(300,300), //winSize, tamanho das imagens
+    Size(40,40), //blocksize, lida com variacoes de iluminacao (2 x cellSize - normalmente)
+    Size(20,20), //blockStride, controla o grau de normalização do contraste (0.5 x blockSize)
+    Size(20,20), //cellSize, tamanho da descritor que é escolhido com base na escala dos recursos importantes para fazer a classificação 
+             9, //nbins
+              1, //derivAper
+             -1, //winSigma
+              0, //histogramNormType
+            0.2, //L2HysThresh
+              1,//gammal correction
+              64,//nlevels=64
+              0//Use signed gradients 
+);
+
+//Cria os descritores HOG das imagens
+void createHOG(vector<float> &HOG, Mat &image){
+ 
+    cvtColor( image, image, CV_BGR2GRAY );
+    //elimina ruidos pelo metodo gaussiano 
+    GaussianBlur(image, image, Size(3,3), 0, 0, BORDER_DEFAULT );
+    
+    hog.compute(image, HOG);      
+}
+
+//Cria os descritores de contorno das imagens
+Mat createEdges(Mat in){
+    Mat image = in;
+    //trandforma a imagem para grayscale
+    //cvtColor(image, image, CV_BGR2GRAY);
+
+    //elimina ruidos pelo metodo gaussiano 
+    //GaussianBlur(image, image, Size(3,3), 0, 0, BORDER_DEFAULT );
+
+    Canny(image, image, 80, 80, 3);
+
+    //usando o operador lapaciano com o fato de que a segunda derivada nas extremidades do objeto sera zero
+    //Laplacian(images[i], edge, CV_16S, 3, 1, 0 , BORDER_DEFAULT);
+    /*
+    int ii = 0; //coluna atual
+    
+    for (int i = 0; i < image.rows; i++){
+        for (int j = 0; j < image.cols; j++){
+            edges.at<float>(0, ii++) = image.at<uchar>(i, j);
+        }
+    } 
+    */
+    return image;    
+}
+
+int main(){
+
+    cout << "******VIZUALIZAÇÂO DO DESCRITOR HOG******\n";
+
+    Mat img = imread("visualizations/img.jpg");
+
+    vector<float> HOG;
+    createHOG(HOG, img);
+
+    cout << "Tamanho do descritor: " << HOG.size() << endl;
+
+    Mat visu = get_hogdescriptor_visu( img, HOG, Size(300, 300) );
+
+    imwrite("visualizations/hog_visu.png", visu);
+    
+    Mat edges = createEdges(img);
+
+    imwrite("visualizations/edge_visu.png", edges);
+
+    cout << "Imagens salvas!\n";
+    //imshow("Descritor HOG", visu);
+    //waitKey(0);
+
+    return 0;
+}
