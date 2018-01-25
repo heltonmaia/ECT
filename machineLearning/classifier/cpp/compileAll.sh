1851e7278eadcbbb1b7e20c391e1bc6b5c569d42
vanluwin <richardson-santiago@hotmail.com>
2018-01-25 13:49:42 -0300

    compileAll script

--- machineLearning/classifier/cpp/compileAll.sh
+++ machineLearning/classifier/cpp/compileAll.sh
@@ -1,8 +1,9 @@
-echo "Compilando tudo ..."
+echo "Compiling..."
 g++ -ggdb -std=c++11 train.cpp -o train `pkg-config --cflags --libs opencv`
 g++ -ggdb -std=c++11 test.cpp -o test `pkg-config --cflags --libs opencv`
 g++ -ggdb -std=c++11 hog.cpp -o hog `pkg-config --cflags --libs opencv`
 g++ -ggdb -std=c++11 edges.cpp -o edges `pkg-config --cflags --libs opencv`
+echo "Done!"
 
 #g++ -ggdb -std=c++11 sampling/samples_hog.cpp -o sampling/hog `pkg-config --cflags --libs opencv`
 #g++ -ggdb -std=c++11 sampling/samples_test.cpp -o sampling/test `pkg-config --cflags --libs opencv`
