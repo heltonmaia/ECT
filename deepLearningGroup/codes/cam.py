32a48cc1bce3a7d4ba95df6a5260401774034b14
Helton Maia <heltonmaia@users.noreply.github.com>
2018-03-08 22:35:27 -0300

    Update cam.py

--- deepLearningGroup/codes/cam.py
+++ deepLearningGroup/codes/cam.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
-
+"""
+by helton
+"""
 import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
 
 while(True):
-    # Capture frame-by-frame
+    # Capture camera frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
+    # show live video
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+# release the capture
 cap.release()
 cv2.destroyAllWindows()
