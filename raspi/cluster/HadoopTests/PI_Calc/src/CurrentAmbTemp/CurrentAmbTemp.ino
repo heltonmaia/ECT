34a9dd23fe0b7ba29450283b91b6a1324a9629bd
Hemerson R. P. Pontes <rafaelpontes1995@gmail.com>
2018-08-19 00:15:28 -0300

    Update CurrentAmbTemp.ino

--- raspi/cluster/HadoopTests/PI_Calc/src/CurrentAmbTemp/CurrentAmbTemp.ino
+++ raspi/cluster/HadoopTests/PI_Calc/src/CurrentAmbTemp/CurrentAmbTemp.ino
@@ -95,7 +95,7 @@ void loop() {
       // open the file. note that only one file can be open at a time,
       // so you have to close this one before opening another.
       MyFile = SD.open("leituras.csv", FILE_WRITE);
-      
+      nameFile="";
       nameFile.concat("Current_AmbTemp_");
       nameFile.concat(String(myRTC.dayofmonth));
       nameFile.concat( ".");
